64c419dbbed63e5aea311e368cab0a02b7145c68
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-14 19:15:12 +0300

    Exclamation marks series

---
+++ 8kyu/exclamation-marks-series.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+//SOLUTION:
+
+function replace(s) {
+    let word = Array.from(s);
+    for (let i = 0; i < s.length; i++) {
+    if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u' ||
+    word[i] === 'A' || word[i] === 'E' || word[i] === 'I' || word[i] === 'O' || word[i] === 'U') {
+      word[i] = '!'
+      }
+    } return word.join('')
+      }
