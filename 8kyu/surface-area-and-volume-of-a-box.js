1833c0fcb005fa038accaf00ce152de51c73c133
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-29 11:56:51 +0300

    Surface Area and Volume of a Box

---
+++ 8kyu/surface-area-and-volume-of-a-box.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+
+
+
+//SOLUTION:
+
+function getSize(width, height, depth) {
+    return [width * height * 2 + width * depth * 2 + height * depth * 2, width * height * depth]
+}
