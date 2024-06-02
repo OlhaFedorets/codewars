242cd479702a961c9459efdf901953ff0ba5db0a
OlhaFedorets <fedoretsolha@gmail.com>
2024-06-02 12:18:04 +0300

    How long should you cook this for?

---
+++ 7kyu/how-long-should-you-cook-this-for.js
@@ -0,0 +1,52 @@
+// DESCRIPTION:
+// You've purchased a ready-meal from the supermarket.
+
+// The packaging says that you should microwave it for 4 minutes and 20 seconds, based on a 600W microwave.
+
+// Oh no, your microwave is 800W! How long should you cook this for?!
+
+// Input
+// You'll be given 4 arguments:
+
+// 1. needed power
+// The power of the needed microwave.
+// Example: "600W"
+
+// 2. minutes
+// The number of minutes shown on the package.
+// Example: 4
+
+// 3. seconds
+// The number of seconds shown on the package.
+// Example: 20
+
+// 4. power
+// The power of your microwave.
+// Example: "800W"
+
+// Output
+// The amount of time you should cook the meal for formatted as a string.
+// Example: "3 minutes 15 seconds"
+
+// Note: the result should be rounded up.
+
+// 59.2 sec  -->  60 sec  -->  return "1 minutes 0 seconds"
+
+
+
+
+
+
+//SOLUTION:
+
+function cookingTime(neededPower, minutes, seconds, power) {
+
+    const time = (parseInt(neededPower.match(/\d+/g).join('')) / parseInt(power.match(/\d+/g).join('')) * (minutes + seconds / 60));
+    let min = Math.floor(time);
+    let sec = Math.ceil((time - min) * 60);
+    if (sec === 60) {
+        min += 1
+        sec = 0
+    }
+    return min + " minutes " + sec + " seconds"
+}
