1b493aba943cb2f66c109c99a069436fa2437e01
OlhaFedorets <fedoretsolha@gmail.com>
2024-06-06 20:07:03 +0300

    Thinking & Testing : How many "word"?

---
+++ 7kyu/thinking-&-testing-how-many-word.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at the results of the testcases, and guess the code!
+
+
+
+
+
+
+//SOLUTION:
+
+function testit(s) {
+    return (s.match(/w.*?o.*?r.*?d/gi) || []).length
+}
