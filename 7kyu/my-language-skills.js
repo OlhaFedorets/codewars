52d645812b883ecb86a5cb99468f93de387602b3
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-09 14:30:00 +0300

    My Language Skills

--- 7kyu/my-language-skills.js
+++ 7kyu/my-language-skills.js
@@ -12,8 +12,13 @@
 
 
 
-//SOLUTION:
+//SOLUTIONS:
 
 function myLanguages(results) {
     return Object.entries(results).sort((a, b) => b[1] - a[1]).map((el) => el[1] >= 60 ? el[0] : 0).filter(Boolean)
+}
+
+
+function myLanguages(results) {
+    return Object.entries(results).filter((el) => el[1] >= 60).sort((a, b) => b[1] - a[1]).map((i) => i[0])
 }
