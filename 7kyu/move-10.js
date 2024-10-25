acaffe4904a27c6a84395a02af20f6007ee50309
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-25 10:25:24 +0300

    move-10.js

--- 7kyu/move-10.js
+++ 7kyu/move-10.js
@@ -1,3 +1,15 @@
+// Description:
+// Move every letter in the provided string forward 10 letters through the alphabet.
+
+// If it goes past 'z', start again at 'a'.
+
+// Input will be a string with length > 0.
+
+
+
+
+
+
 function moveTen(s) {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     let result = []
