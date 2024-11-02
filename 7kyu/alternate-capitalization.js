1260075a1fd7ab28e9705dbfe369897833ae1157
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-02 17:26:43 +0300

    Alternate capitalization

---
+++ 7kyu/alternate-capitalization.js
@@ -0,0 +1,31 @@
+// Description:
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, 
+//and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+
+
+
+
+//SOLUTION:
+
+
+function capitalize(s) {
+    let firstStr = ''
+    let secondStr = ''
+    for (i = 0; i < s.length; i++) {
+        if (i === 0 || i % 2 === 0) {
+            firstStr += s[i].toUpperCase()
+            secondStr += s[i]
+        } else {
+            firstStr += s[i]
+            secondStr += s[i].toUpperCase()
+        }
+    }
+    return [firstStr, secondStr]
+};
