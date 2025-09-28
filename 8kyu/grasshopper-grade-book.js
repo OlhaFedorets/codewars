8e792424d58df088e7d8416cb5a0e2d69d7565f6
OlhaFedorets <fedoretsolha@gmail.com>
2025-09-28 14:19:03 +0300

    Grasshopper - Grade book

---
+++ 8kyu/grasshopper-grade-book.js
@@ -0,0 +1,25 @@
+//Description: 
+
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it 
+// and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+
+
+
+
+//Solution:
+
+function getGrade(s1, s2, s3) {
+    const average = (s1 + s2 + s3) / 3
+    return average >= 90 ? 'A' :
+        average >= 80 ? 'B' :
+            average >= 70 ? 'C' :
+                average >= 60 ? 'D' : 'F'
+}
