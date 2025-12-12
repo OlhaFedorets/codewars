02f30b87ce03411213b80fde4e9e77976bc48050
OlhaFedorets <fedoretsolha@gmail.com>
2025-12-12 12:27:33 +0300

    Broken Counter

---
+++ 8kyu/broken-counter.js
@@ -0,0 +1,22 @@
+// Description:
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+
+
+//Solution:
+
+function Counter() {
+    return this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+    this.value++;
+};
+
+Counter.prototype.getValue = function () {
+    return this.value;
+};
+
+Counter.prototype.reset = function () {
+    this.value = 0;
+};
